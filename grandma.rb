248391244bca8d33a24370f525c11d75a7d6fce3
Nicholas Walton <cactar65@gmail.com>
2019-10-08 14:21:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -22,7 +22,8 @@ def speak_to_grandma(phrase)
   elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
     
-  elsif phrase == ""
+  elsif phrase == "Hi!"
+    return ""
     
   end
 end
